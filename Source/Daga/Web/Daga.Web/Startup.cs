d205cccd118f84d93730b21938aef088cd43e2ae
Borayvor <boray@abv.bg>
2016-02-04 15:13:28 +0200

    Create Daga.Web project

---
+++ Source/Daga/Web/Daga.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Daga.Web.Startup))]
+namespace Daga.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
