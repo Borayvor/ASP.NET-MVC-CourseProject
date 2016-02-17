00c8aefa5cfb1e547ce6210a16b07e9672499907
Borayvor <boray@abv.bg>
2016-02-17 22:50:07 +0200

    add Crossword, Word and Question Models

---
+++ Source/Data/InteractiveLearningSystem.Data.Models/Question.cs
@@ -0,0 +1,19 @@
+﻿namespace InteractiveLearningSystem.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Question
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(1)]
+        [MaxLength(500)]
+        public string Name { get; set; }
+
+        public int WordId { get; set; }
+
+        public virtual Word Word { get; set; }
+    }
+}
