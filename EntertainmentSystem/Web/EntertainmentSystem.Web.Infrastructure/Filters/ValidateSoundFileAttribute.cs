7c2bb22a8c8cf0c826f2b04c0f50ce7316bc63e5
Borayvor <boray@abv.bg>
2016-06-14 19:05:29 +0300

    add media filters

---
+++ EntertainmentSystem/Web/EntertainmentSystem.Web.Infrastructure/Filters/ValidateSoundFileAttribute.cs
@@ -0,0 +1,25 @@
+﻿namespace EntertainmentSystem.Web.Infrastructure.Filters
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class ValidateSoundFileAttribute : BaseValidateMediaFileAttribute
+    {
+        private readonly IList<string> allowedMimeTypes = new List<string>() { "audio/mpeg", "audio/wav", "audio/mp3" };
+
+        public override bool IsValid(object value)
+        {
+            try
+            {
+                this.ValidateOrThrowException(value, 1024 * 1024 * 10 /*10MB*/, this.allowedMimeTypes);
+            }
+            catch (Exception ex)
+            {
+                this.ErrorMessage = ex.Message;
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
