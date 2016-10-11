503a0d495f0d8205dd24a03adee2f12662776a36
Borayvor <boray@abv.bg>
2016-10-11 20:30:14 +0300

    add several forum-service interfaces -> start

---
+++ EntertainmentSystem/Services/EntertainmentSystem.Services.Contracts/Forum/IForumTagService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntertainmentSystem.Services.Contracts.Forum
+{
+    interface IForumTagService
+    {
+    }
+}
