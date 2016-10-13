58395568dc3e079ea555a57280339b9e78075a17
Borayvor <boray@abv.bg>
2016-10-13 18:39:09 +0300

    refactor BaseModelGuid

--- EntertainmentSystem/Data/EntertainmentSystem.Data.Common/Models/BaseModelGuid.cs
+++ EntertainmentSystem/Data/EntertainmentSystem.Data.Common/Models/BaseModelGuid.cs
@@ -1,26 +1,12 @@
 ﻿namespace EntertainmentSystem.Data.Common.Models
 {
     using System;
-    using System.ComponentModel.DataAnnotations;
-    using System.ComponentModel.DataAnnotations.Schema;
 
-    public abstract class BaseModelGuid : IAuditInfo, IDeletableEntity
+    public abstract class BaseModelGuid : BaseModel<Guid>
     {
         public BaseModelGuid()
         {
             this.Id = Guid.NewGuid();
         }
-
-        [Key]
-        public Guid Id { get; set; }
-
-        public DateTime CreatedOn { get; set; }
-
-        public DateTime? ModifiedOn { get; set; }
-
-        [Index]
-        public bool IsDeleted { get; set; }
-
-        public DateTime? DeletedOn { get; set; }
     }
 }
