61220954b6dd40e6d5d27e0a75c521915ac1a008
Borayvor <boray@abv.bg>
2016-10-28 00:05:47 +0300

    refactor upload html and css

--- EntertainmentSystem/Web/EntertainmentSystem.Web/Scripts/App/EntertainmentSystemApp.js
+++ EntertainmentSystem/Web/EntertainmentSystem.Web/Scripts/App/EntertainmentSystemApp.js
@@ -1,12 +1,9 @@
-﻿$(function () {
+﻿
+$(function () {
     // upload Music content
     $('.chooseUploadMusicBtn').click(function (e) {
         e.preventDefault();
         $('.inputUploadMusicBtn').click();
-    });
-
-    $('.sendUploadMusicBtn').click(function (e) {        
-        $(this).parent().children(".es-upload-btn").click();
     });
 
     // upload Picture content
@@ -15,18 +12,10 @@
         $('.inputUploadPictureBtn').click();
     });
 
-    $('.sendUploadPictureBtn').click(function (e) {        
-        $(this).parent().children(".es-upload-btn").click();
-    });        
-
     // upload Video content
     $('.chooseUploadVideoBtn').click(function (e) {
         e.preventDefault();
         $('.inputUploadVideoBtn').click();
-    });
-
-    $('.sendUploadVideoBtn').click(function (e) {       
-        $(this).parent().children(".es-upload-btn").click();
     });
 
     // test   
