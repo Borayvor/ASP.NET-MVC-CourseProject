d00f5576ec4217389c32f31e411ad71ba56f7917
Borayvor <boray@abv.bg>
2016-11-03 13:13:45 +0200

    change bundles paths

--- EntertainmentSystem/Web/EntertainmentSystem.Web/App_Start/BundleConfig.cs
+++ EntertainmentSystem/Web/EntertainmentSystem.Web/App_Start/BundleConfig.cs
@@ -21,25 +21,25 @@
 
         private static void RegisterScripts(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundlesJs/jquery")
+            bundles.Add(new ScriptBundle("~/bundles/jquery")
                 .Include(
                     "~/Scripts/KendoUI/jquery.min.js"));
 
-            bundles.Add(new ScriptBundle("~/bundlesJs/jqueryval")
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval")
                 .Include("~/Scripts/jquery.validate*"));
 
-            bundles.Add(new ScriptBundle("~/bundlesJs/kendo")
+            bundles.Add(new ScriptBundle("~/bundles/kendo")
                 .Include(
                     "~/Scripts/KendoUI/kendo.all.min.js",
                     "~/Scripts/KendoUI/kendo.aspnetmvc.min.js"));
 
-            bundles.Add(new ScriptBundle("~/bundlesJs/tools")
+            bundles.Add(new ScriptBundle("~/bundles/tools")
                 .Include(
                 "~/Scripts/bootstrap.js",
                 "~/Scripts/jquery.fancybox.js",
                 "~/Scripts/toastr.js"));
 
-            bundles.Add(new ScriptBundle("~/bundlesJs/appJs")
+            bundles.Add(new ScriptBundle("~/bundles/appJs")
                 .Include(
                 "~/Scripts/App/EntertainmentSystemApp.js",
                 "~/Scripts/App/EntertainmentSystem-media.js"));
@@ -47,17 +47,17 @@
 
         private static void RegisterStyles(BundleCollection bundles)
         {
-            bundles.Add(new StyleBundle("~/bundlesContent/siteCss")
+            bundles.Add(new StyleBundle("~/Content/siteCss")
                 .Include("~/Content/Site/site.css"));
 
-            bundles.Add(new StyleBundle("~/bundlesContent/tools")
+            bundles.Add(new StyleBundle("~/Content/tools")
                 .Include(
                 "~/Content/bootstrap.css",
                 "~/Content/font-awesome.css",
                 "~/Content/toastr.css",
                 "~/Content/jquery.fancybox.css"));
 
-            bundles.Add(new StyleBundle("~/bundlesContent/kendo")
+            bundles.Add(new StyleBundle("~/Content/KendoUI/kendo")
                 .Include(
                 "~/Content/KendoUI/kendo.common.min.css",
                 "~/Content/kendo/kendo.common-bootstrap.min.css",
