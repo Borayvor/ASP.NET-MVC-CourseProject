15380722ad12c1c0db59ae1d8104cba96f24c519
Borayvor <Boray@abv.bg>
2016-11-22 15:31:01 +0200

    start to add post comments pagination

---
+++ EntertainmentSystem/Web/EntertainmentSystem.Web/Areas/Forum/Controllers/CommentsController.cs
@@ -0,0 +1,54 @@
+﻿namespace EntertainmentSystem.Web.Areas.Forum.Controllers
+{
+    using System;
+    using System.Linq;
+    using System.Web.Mvc;
+    using Common.Constants;
+    using Infrastructure.Mapping;
+    using Services.Contracts.Forum;
+    using ViewModels;
+    using Web.Controllers;
+
+    public class CommentsController : BaseController
+    {
+        private readonly IForumCommentService commentService;
+
+        public CommentsController(IForumCommentService commentService)
+        {
+            this.commentService = commentService;
+        }
+
+        [ChildActionOnly]
+        public ActionResult PostComments(Guid postId)
+        {
+            return this.PartialView();
+        }
+
+        private PostCommentsPageViewModel GetCommentsPage(
+            Guid id,
+            int page = GlobalConstants.ForumStartPage)
+        {
+            var comments = this.commentService.GetAll().Where(c => c.PostId == id);
+
+            int totalpages = 0;
+            int pagesToSkip = (page - 1) * GlobalConstants.ForumCommentsPerPage;
+
+            totalpages = (int)Math.Ceiling(comments.Count() / (decimal)GlobalConstants.ForumCommentsPerPage);
+
+            var result = comments
+                .Skip(pagesToSkip)
+                .Take(GlobalConstants.ForumCommentsPerPage)
+                .To<CommentViewModel>()
+                .ToList();
+
+            var newViewModel = new PostCommentsPageViewModel
+            {
+                Comments = result,
+                CurrentPage = page,
+                TotalPages = totalpages
+            };
+
+            return newViewModel;
+        }
+    }
+}
---
+++ EntertainmentSystem/Web/EntertainmentSystem.Web/Areas/Forum/ViewModels/PostCommentsPageViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace EntertainmentSystem.Web.Areas.Forum.ViewModels
+{
+    using System.Collections.Generic;
+
+    public class PostCommentsPageViewModel
+    {
+        public IEnumerable<CommentViewModel> Comments { get; set; }
+
+        public int TotalPages { get; set; }
+
+        public int CurrentPage { get; set; }
+    }
+}
